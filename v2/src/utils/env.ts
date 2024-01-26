8541ba3e79dbf65eface2fca90a122e49a5a7714
caro1xxx <caro1xxxhv@gmail.com>
2024-01-27 03:48:57 +0800

    build

--- v2/src/utils/env.ts
+++ v2/src/utils/env.ts
@@ -1,2 +1,4 @@
-export const HOST = "http://192.168.31.80";
-export const QRCODEURL = "http://192.168.31.80";
+// export const HOST = "http://192.168.31.80";
+// export const QRCODEURL = "http://192.168.31.80";
+export const HOST = "http://zoomm.cloud";
+export const QRCODEURL = "http://zoomm.cloud";
